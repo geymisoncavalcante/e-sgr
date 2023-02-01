0f66a3936f34e8f058224f695bf3bae8a1b03f79
Geymison S. Cavalcante <geymison.cavalcante@pm.pe.com.br>
2023-02-01 20:02:56 -0300

    validações

---
+++ views/pages/validacao/validations.js
@@ -0,0 +1,27 @@
+const Joi = require('joi')
+
+const userSchema = Joi.object({
+    username: Joi.string()
+        .alphanum()
+        .min(3)
+        .max(30)
+        .required(),
+
+    password: Joi.string()
+        .pattern(/^[a-zA-Z0-9]{3,30}$/),
+
+    birth_year: Joi.number()
+        .integer()
+        .min(1900)
+        .max(2001),
+
+    email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+});
+
+const valida = 
+
+
+module.exports = {
+    userSchema
+}
